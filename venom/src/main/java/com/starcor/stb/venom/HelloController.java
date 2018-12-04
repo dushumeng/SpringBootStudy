b4c5ddc186b15dec5fabd566fa3b4ca9f5d036d7
shmdu <dushumeng@gmail.com>
2018-12-04 18:52:47 +0800

    init

--- venom/src/main/java/com/starcor/stb/venom/HelloController.java
+++ venom/src/main/java/com/starcor/stb/venom/HelloController.java
@@ -1,18 +1,13 @@
 package com.starcor.stb.venom;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Date;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String hello(Model m) {
-        m.addAttribute("now", new Date().toString());
-        return "hello";
+    public String hello() {
+        return "test";
     }
-
 }
